7e9ac5c0e0ef0725fee6fa9ed8bd36fadb7b6cde
Shawn-Bellazan-jr <shawn_bellazan@outlook.com>
2024-10-24 07:13:47 -0500

    updatae

---
+++ Example/Example.AppHost/Program.cs
@@ -0,0 +1,14 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.Example_ApiService>("apiservice");
+
+builder.AddProject<Projects.Example_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(cache)
+    .WithReference(apiService);
+
+builder.AddProject<Projects.OpenAI_Api>("open-ai-api");
+
+builder.Build().Run();
